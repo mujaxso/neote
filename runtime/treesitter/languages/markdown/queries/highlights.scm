70d5f3ed7f135e72e3c209dd0f4a16bbe26f66ef
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 21:51:22 +0200

    fix: update markdown tree-sitter query with compatible node types

--- runtime/treesitter/languages/markdown/queries/highlights.scm
+++ runtime/treesitter/languages/markdown/queries/highlights.scm
@@ -1,33 +1,63 @@
 
 ; Markdown highlighting for tree-sitter-markdown-inline
-; Using node types that actually exist in the grammar
+; Using patterns compatible with the inline variant
 
-; Headings
-(atx_heading) @heading
-(setext_heading) @heading
+; Headings - try different possible node names
+[
+  (atx_heading)
+  (setext_heading)
+  (atx_h1_marker)
+  (atx_h2_marker)
+  (atx_h3_marker)
+  (atx_h4_marker)
+  (atx_h5_marker)
+  (atx_h6_marker)
+  (setext_h1_underline)
+  (setext_h2_underline)
+] @heading
 
 ; Emphasis
-(emphasis) @emphasis
-(strong_emphasis) @strong
+[
+  (emphasis)
+  (strong_emphasis)
+  (emphasis_marker)
+  (strong_emphasis_marker)
+] @emphasis
 
 ; Links
-(link) @link
-(link_text) @link
+[
+  (link)
+  (inline_link)
+] @link
+[
+  (link_text)
+  (link_title)
+] @link
 (link_destination) @string
-(link_title) @string
 
 ; Code
-(inline_code_span) @inline_code
-(code_fence) @code_fence
-(code_span) @inline_code
-(fenced_code_block) @code_fence
+[
+  (inline_code_span)
+  (code_span)
+] @inline_code
+[
+  (code_fence)
+  (fenced_code_block)
+  (code_fence_content)
+] @code_fence
 
 ; Block quotes
-(block_quote) @block_quote
+[
+  (block_quote)
+  (block_quote_marker)
+] @block_quote
 
 ; Lists
-(list) @list
-(list_item) @list
+[
+  (list)
+  (list_item)
+  (list_marker)
+] @list
 (task_list_marker) @operator
 
 ; Thematic break
@@ -40,12 +70,16 @@
 (image) @link
 
 ; References
-(reference_link) @link
-(reference_definition) @link
+[
+  (reference_link)
+  (reference_definition)
+] @link
 
 ; Footnotes
-(footnote_reference) @link
-(footnote_definition) @link
+[
+  (footnote_reference)
+  (footnote_definition)
+] @link
 
 ; Strikethrough
 (strikethrough) @emphasis
@@ -54,19 +88,33 @@
 (escape_sequence) @string
 
 ; Line breaks
-(hard_line_break) @operator
-(soft_line_break) @paragraph
+[
+  (hard_line_break)
+  (soft_line_break)
+] @operator
 
 ; Tables
-(table) @table
-(table_header) @heading
-(table_row) @table
-(table_cell) @paragraph
+[
+  (table)
+  (table_header)
+  (table_row)
+  (table_cell)
+] @table
 
 ; HTML
-(html_block) @html
-(html_inline) @html
+[
+  (html_block)
+  (html_inline)
+] @html
 
 ; URLs and emails
-(url) @string
-(email) @string
+[
+  (url)
+  (email)
+] @string
+
+; Generic text nodes for fallback
+[
+  (text)
+  (inline)
+] @plain
