b16ece03e7420471966b2358bb0014ae79eda404
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 23:03:31 +0200

    fix: remove invalid tree-sitter markdown injection patterns

--- runtime/treesitter/languages/markdown/queries/injections.scm
+++ runtime/treesitter/languages/markdown/queries/injections.scm
@@ -1,12 +1,15 @@
-((html_tag) @injection.content
-  (#set! injection.language "html"))
-
-((latex_block) @injection.content
-  (#set! injection.language "latex"))
 ; Markdown injections for fenced code blocks
 (fenced_code_block
   (info_string) @injection.language
   (code_fence_content) @injection.content)
+
+; HTML injection if html_tag exists
+; ((html_tag) @injection.content
+;   (#set! injection.language "html"))
+
+; LaTeX injection if latex_block exists  
+; ((latex_block) @injection.content
+;   (#set! injection.language "latex"))
 
 ; Support for inline code if needed
 ; (code_span) @injection.content
